3b7f713eec6c020f08628c33ce5e2d9b16206939
Ed <edward.blewitt2@gmail.com>
2025-01-20 19:02:37 +0000

    Headings wont get too big on mobile and added meta theme-color

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#232323">
 
     <!-- Insert any custom head contents -->
     <?php echo $page->head_content ?>
